89a1c8a5fa81c86f72ad244a5ed3adf4fe51b14f
Gantam Manish Kumar <praneethraj111@gmail.com>
2026-05-04 13:18:00 +0530

    Completed SQLBolt Lesson 1 Tasks 1-5

---
+++ SQLBolt/lesson_01_select.sql
@@ -0,0 +1,16 @@
+-- SQLBolt lesson 1: select queries
+
+-- Task 1 : Find the title of each film
+SELECT title FROM movies;
+
+-- Task 2 : Find the director of each film
+SELECT director FROM movies;
+
+-- Task 3 : Find the title and director of each film
+SELECT title,director FROM movies;
+
+-- Task 4 : Find the title and year of each film
+SELECT title,year FROM movies;
+
+--Task 5 : Find all the information about each film
+SELECT * FROM movies;
