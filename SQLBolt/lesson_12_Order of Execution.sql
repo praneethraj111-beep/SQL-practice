d1ff099133ac4cfdfb55dadf28dc6c7ea1eee9e6
Gantam Manish Kumar <praneethraj111@gmail.com>
2026-05-07 15:00:15 +0530

    Completed SQLBolt Lesson 11 Aggregates_queries.sql

--- SQLBolt/lesson_12_Order of Execution.sql
+++
@@ -1,6 +0,0 @@
--- SQLBolt Lesson 12 : -- Order of execution
--- Task 1 : Find the number of movies each director has directed
-SELECT director, COUNT(id) as Num_movies_directed FROM movies GROUP BY director;
-
--- Task 2 : -- Find the total domestic and international sales that can be attributed to each director
-SELECT Director, SUM(domestic_sales + international_sales) as cumulative_sales_from_all_movies FROM movies INNER JOIN boxoffice ON movies.id = boxoffice.movie_id GROUP BY Director;
