8577e9020cfce97817ad86be758b41ae9258f842
Gantam Manish Kumar <praneethraj111@gmail.com>
2026-05-07 21:51:21 +0530

    Completed

--- SQLBolt/lesson _17_Altering_tables.sql
+++
@@ -1,7 +0,0 @@
--- SQLBolt Lesson 17 : -- Altering tables
-
--- Task : -- Add a column named Aspect_ratio with a FLOAT data type to store the aspect-ratio each movie was released in.
-ALTER TABLE Movies ADD COLUMN Aspect_ratio FLOAT DEFAULT 2.39;
-
--- Task : -- Add another column named Language with a TEXT data type to store the language that the movie was released in. Ensure that the default for this language is English.
-ALTER TABLE Movies ADD COLUMN Language TEXT DEFAULT "English";
