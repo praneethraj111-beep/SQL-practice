70e1ba022ddda530cdf04f0db8f44381be32daad
Gantam Manish Kumar <praneethraj111@gmail.com>
2026-05-07 21:43:56 +0530

    Completed SQLBolt 18 Dropping Tables

---
+++ SQLBolt/lesson_18_Dropping_tables.sql
@@ -0,0 +1,7 @@
+-- SQLBolt Lesson 18 : -- Dropping tables
+
+-- Task 1 : -- We've sadly reached the end of our lessons, lets clean up by removing the Movies table
+DROP TABLE IF EXISTS Movies;
+
+-- Task 2 : -- And drop the BoxOffice table as well
+DROP TABLE IF EXISTS boxoffice;
