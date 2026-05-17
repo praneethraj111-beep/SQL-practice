971a35e438febf048c5583a3ce487000ee684dcc
Gantam Manish Kumar <praneethraj111@gmail.com>
2026-05-17 13:39:38 +0530

    basics learning

---
+++ PROJECTS/PulsePoint Healthcare Analytics/python_basics.py
@@ -0,0 +1,20 @@
+#1. Strings (Text data) - Always wrapped in quotes
+project_name = "PulsePoint Healthcare"
+manager_name = 'Manish'
+
+#2. Integers (Whole numbers)
+total_patients = 5
+
+#3. Floats (Decimal numbers)
+total_billing = 30500.50
+
+#4. Booleans (True/False decisions)
+pipeline_success = True
+
+# 5. Basic math
+average_bill_per_patient = total_billing / total_patients
+# CHANGE THIS LINE TO ADD A LABEL:
+print(f"Average Bill per Patient: {average_bill_per_patient}")
+
+# 6. The Type Trap
+print(f"Project: {project_name} | Total Patients: {total_patients}")
