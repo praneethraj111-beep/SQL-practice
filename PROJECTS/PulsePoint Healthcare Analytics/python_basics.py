051e6ca4d24e9d917baedd1df4b96d64cc0cac0f
Gantam Manish Kumar <praneethraj111@gmail.com>
2026-05-18 14:21:32 +0530

    completed python basics queries

--- PROJECTS/PulsePoint Healthcare Analytics/python_basics.py
+++ PROJECTS/PulsePoint Healthcare Analytics/python_basics.py
@@ -18,3 +18,32 @@ print(f"Average Bill per Patient: {average_bill_per_patient}")
 
 # 6. The Type Trap
 print(f"Project: {project_name} | Total Patients: {total_patients}")
+
+# 7. Lists and Decision Making (Control Flow)
+# A list of patient billing amounts (Data Collection)
+patient_bills = [1200.50, 4500.00, 8500.25, 9500.00]
+
+print("\n--- PATIENT BILLING AUDIT")
+
+# Loop through each bill in our list
+for bill in patient_bills:
+    # Decision Making: Check if the bill exceeds a premium threshold
+    if bill > 5000.00:
+        print(f"Alert: High-Value Account detected! Bill: ${bill}")
+    else:
+        print(f"Normal Account. Bill: ${bill}")
+
+       # Line 36: The loop starts here
+for bill in patient_bills:
+    # Line 38: Pushed right by 4 spaces (1 Tab)
+    if bill > 7000.00:
+        # Line 39: Pushed right by 8 spaces (2 Tabs) because it belongs to 'if'
+        print(f"Alert: High-Value Account! Bill: ${bill}")
+    # Line 40: Aligned exactly with the 'if' statement
+    elif bill > 4000.00:
+        # Line 41: Pushed right by 8 spaces
+        print(f"Warning: Medium-Value Account. Bill: ${bill}")
+    # Line 42: Aligned exactly with 'if' and 'elif'
+    else:
+        # Line 43: Pushed right by 8 spaces
+        print(f"Normal Account. Bill: ${bill}")
